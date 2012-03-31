8d00926d1919e4d1f8b15983de9873dbc9b9b08f
Michael Harris <mic2957@msn.com>
2012-03-31 03:21:22 -0400

    Forgot some files

---
+++ src/Moxy/Moxy/Entities/Animation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Linq;
+
+namespace Moxy.Entities
+{
+
+	public class Animation
+	{
+		private static readonly TimeSpan DefaultTimeSpan = new TimeSpan(0, 0, 0, 0, 500);
+
+		public readonly TimeSpan FrameRate;
+		public readonly string Name;
+		public readonly Rectangle[] Sources;
+		public int CurrentSource;
+
+		public Animation(string name, IEnumerable<Rectangle> sources, TimeSpan frameRate = default(TimeSpan))
+		{
+			Name = name;
+			Sources = sources.ToArray();
+			FrameRate = frameRate == default(TimeSpan) ? DefaultTimeSpan : frameRate;
+		}
+	}
+
+	public class AnimationManager
+	{
+		public AnimationManager(Texture2D texture2D, Rectangle lRectangle, IEnumerable<Animation> animationsList = null)
+		{
+			animations = animationsList != null ? new Dictionary<string, Animation>(animationsList.ToDictionary((ani) => ani.Name)) : new Dictionary<string, Animation>();
+		}
+
+		public AnimationManager(Texture2D texture2D, IEnumerable<Animation> animationsList = null)
+		{
+			animations = animationsList != null ? new Dictionary<string, Animation>(animationsList.ToDictionary((ani) => ani.Name)) : new Dictionary<string, Animation>();
+
+		}
+
+		public void AddAnimation(Animation animation)
+		{
+			animations[animation.Name] = animation;
+		}
+
+		public bool SetAnimation(string Name)
+		{
+			Animation animation;
+			if (!animations.TryGetValue(Name, out animation))
+				return false;
+
+			currentAnimation = animation;
+			currentAnimation.CurrentSource = 0;
+			Bounding = currentAnimation.Sources[0];
+			return true;
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			if (currentAnimation != null)
+				playAnimation(gameTime);
+		}
+
+		private void playAnimation(GameTime gameTime)
+		{
+			lastUpdate += gameTime.ElapsedGameTime;
+			if (lastUpdate < currentAnimation.FrameRate)
+				return;
+
+			currentAnimation.CurrentSource = ((currentAnimation.CurrentSource + 1) % currentAnimation.Sources.Length);
+			Bounding = currentAnimation.Sources[currentAnimation.CurrentSource];
+			lastUpdate = TimeSpan.Zero;
+		}
+
+		public Rectangle Bounding;
+
+		private TimeSpan lastUpdate = TimeSpan.Zero;
+		private Animation currentAnimation;
+		private readonly Dictionary<string, Animation> animations;
+	}
+}
---
+++ src/Moxy/Moxy/GameStates/MapPlayerState.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moxy.Map;
+using Moxy.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Moxy.GameStates
+{
+	public class MapPlayerState
+		: BaseGameState
+	{
+		public MapPlayerState()
+			: base("MapPlayerState")
+		{
+			Map = new TileMap();
+			Dudes = new List<Player>();
+			Dudes.Add(new PowerGenerator()
+			{
+				Team = Team.Blue,
+				Location = new Vector2(0, 0),
+				Color = Color.White,
+				Speed = .5f,
+				PadIndex = PlayerIndex.One
+			});
+			Camera = new Camera(Moxy.ScreenWidth, Moxy.ScreenHeight);
+			Map.Camera = Camera;
+			//Camera.UseBounds = true;
+			//Camera.Bounds = new Rectangle(0, 0, 1600, 1600);
+			
+		}
+
+
+		public override void Update(GameTime gameTime)
+		{
+			Map.Update(gameTime);
+			Dudes.ForEach(dude => dude.Update(gameTime));
+			if (!Map.EditActive)
+				Camera.CenterOnPoint(Dudes[0].Location);
+		}
+
+		public override void Draw(SpriteBatch batch)
+		{
+			batch.Begin(SpriteSortMode.Texture, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullCounterClockwise, null, Camera.Transformation);
+			Map.Draw(batch);
+			batch.End();
+			batch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullCounterClockwise, null, Camera.Transformation);
+			Dudes.ForEach(dude => dude.Draw(batch));
+			batch.End();
+	
+
+
+		}
+
+		public override void Load()
+		{
+			Map.Texture = Moxy.ContentManager.Load<Texture2D>("lofi_tiles");
+			Map.MapSize = new Vector2(50, 50);
+			Map.TileSize = new Vector2(8, 8);
+			Map.CreateTiles();
+		}
+
+		public TileMap Map;
+		public List<Player> Dudes;
+		public Camera Camera;
+	}
+}
